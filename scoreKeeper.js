2bcfd770dc627c78346a864126cbcb8666cf7bc0
sbasnet7227 <basnet_sunil@hotmail.com>
2019-10-21 08:46:53 -0700

    reset button code is working

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -34,8 +34,12 @@ p2Button.addEventListener('click', function(){
 });
 
 resetButton.addEventListener('click', function(){
-    // p1Score = 0;
-    // p2Score = 0;
-    // p1Display.textContent = p1Score;
-    // p2Display.textContent = p2Score;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner');
+    p2Display.classList.remove('winner');
+    gameOver = false;
+   
 });
